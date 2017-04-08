a2abcebe216ef6516afb3ee25347fc328bbc935f
Seagyn <seagyn@digitalleap.co.za>
2017-04-08 23:15:03 +0200

    Fixed signup styles

--- templates/sign-up/sign-up.php
+++ templates/sign-up/sign-up.php
@@ -18,7 +18,7 @@ fp_display_flash_message();
 
 	<h3>Member Details</h3>
 
-	<p class="form-row form-row-first">
+	<p class="form-row form-row-wide">
 		<label for="membership_id"><?php _e( 'Membership', 'fitpress' ); ?> <span class="required">*</span></label>
 		<select name="membership_id" id="membership_id">
 			<?php $passed_membership_id = isset( $_GET['membership_id'] ) ? $_GET['membership_id'] : false;?>
@@ -40,12 +40,12 @@ fp_display_flash_message();
 	</p>
 	<div class="clear"></div>
 
-	<p class="form-row form-row-wide">
+	<p class="form-row form-row-first">
 		<label for="account_email"><?php _e( 'Email address', 'fitpress' ); ?> <span class="required">*</span></label>
 		<input type="email" class="input-text" name="account_email" id="account_email" value="<?php echo esc_attr( isset( $_POST['account_email'] ) ? $_POST['account_email'] : '' ); ?>" />
 	</p>
 
-	<p class="form-row form-row-wide">
+	<p class="form-row form-row-last">
 		<label for="contact_number"><?php _e( 'Contact Number', 'fitpress' ); ?> <span class="required">*</span></label>
 		<input type="tel" class="input-text" name="contact_number" id="contact_number" value="<?php echo esc_attr( isset( $_POST['contact_number'] ) ? $_POST['contact_number'] : '' ); ?>" />
 	</p>
@@ -58,7 +58,7 @@ fp_display_flash_message();
 		<label for="emergency_contact_number"><?php _e( 'Emergency Contact Number', 'fitpress' ); ?> <span class="required">*</span></label>
 		<input type="text" class="input-text" name="emergency_contact_number" id="emergency_contact_number" value="<?php echo esc_attr( isset( $_POST['emergency_contact_number'] ) ? $_POST['emergency_contact_number'] : '' ); ?>" />
 	</p>
-	<div class="clear"></div>
+	<div class="clearfix"></div>
 
 	<h3>Log In Details</h3>
 
@@ -67,18 +67,19 @@ fp_display_flash_message();
 		<input type="text" class="input-text" name="account_username" id="account_username" value="<?php echo esc_attr( isset( $_POST['account_username'] ) ? $_POST['account_username'] : '' ); ?>" />
 	</p>
 
-	<p class="form-row form-row-wide">
+	<p class="form-row form-row-first">
 		<label for="password_1"><?php _e( 'Password', 'fitpress' ); ?> <span class="required">*</span></label>
 		<input type="password" class="input-text" name="password_1" id="password_1" />
 	</p>
-	<p class="form-row form-row-wide">
+	<p class="form-row form-row-last">
 		<label for="password_2"><?php _e( 'Confirm Password', 'fitpress' ); ?> <span class="required">*</span></label>
 		<input type="password" class="input-text" name="password_2" id="password_2" />
 	</p>
+	<div class="clearfix"></div>
 
-	<p>
+	<p class="form-row form-row-submit">
 		<?php wp_nonce_field( 'signup_account' ); ?>
-		<input type="submit" class="btn" name="signup_account" value="<?php _e( 'Sign Up', 'fitpress' ); ?>" />
+		<input type="submit" class="btn button" name="signup_account" value="<?php _e( 'Sign Up', 'fitpress' ); ?>" />
 		<input type="hidden" name="action" value="signup_account" />
 	</p>
 
